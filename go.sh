d79fdb4dc84002cb42bd99de9b7a2147b55b1444
alexandertuna <alexander.tuna@gmail.com>
2025-03-14 13:48:59 -0700

    Remove old executables by default

--- go.sh
+++ go.sh
@@ -1,5 +1,6 @@
 # source ~/miniconda3/bin/activate
 # conda activate xrootd
+rm -f minio mc
 rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
 cmake ./
 rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
