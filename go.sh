e217de7c94a20a9a20d08485662423da1302851c
alexandertuna <alexander.tuna@gmail.com>
2025-03-13 20:44:52 -0700

    Start playing

---
+++ go.sh
@@ -0,0 +1,5 @@
+# source ~/miniconda3/bin/activate
+# conda activate xrootd
+rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
+cmake ./
+rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
